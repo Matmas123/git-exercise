393dd3d1356e6605f5e72710402877c540236c94
ProbstenHias <matthias.weilinger@gmx.de>
2022-11-04 13:40:29 +0100

    Add method that checks if value is in certain interval

--- exercise3.py
+++ exercise3.py
@@ -31,9 +31,11 @@ class Rectangle:
         #         and reuse that here.
         ll_px = point.x - self._lower_left.x
         ll_py = point.y - self._lower_left.y
-        return ll_px >= -tolerance and ll_px <= self._dx + tolerance \
-            and ll_py >= -tolerance and ll_py <= self._dy + tolerance
-
+        return self._is_value_in_interval(ll_px, 0, self._dx, tolerance) \
+            and self._is_value_in_interval(ll_py, 0, self._dy, tolerance)
+    
+    def _is_value_in_interval(self, i: int, min: int, max: int, tolerance: float) -> bool:
+        return i >= min - tolerance and i <= max + tolerance
 
     def _is_idx_on_upper_edge(self, i: int) -> bool:
         return i in [2, 3]
